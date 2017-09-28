134d7f330be6b0ab0c3e76f525a9607010789d1e
redface <redface0512@gmail.com>
2017-09-28 15:17:18 +0900

    model change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  changeTitle(changedTitle) {
+    this.title = changedTitle;
+  }
+
   resetTitle() {
     this.title = 'app';
   }
